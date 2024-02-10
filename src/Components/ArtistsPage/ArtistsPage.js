7ee4b9bb0393376fd065d137ce1c20c77d1e823c
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-02-10 22:19:35 +0200

    artists page

--- src/Components/ArtistsPage/ArtistsPage.js
+++ src/Components/ArtistsPage/ArtistsPage.js
@@ -6,8 +6,24 @@ import "swiper/css/free-mode";
 import "swiper/css/pagination";
 import { Images } from "../LandingPage/Artists/imgs";
 import "./ArtistsPage.css";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function ArtistsPage() {
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/api/chart`).then(function (res) {
+      setArtists(res.data.artists.data);
+      console.log(res.data);
+    });
+  });
+
+  useEffect(() => {
+    console.log(artists);
+  }, [artists]);
+
   return (
     <>
       <Hero />
@@ -15,69 +31,26 @@ export default function ArtistsPage() {
         <div className="section">
           <div className="section__head">
             <h3 className="mb-0">
-              Featured <span className="text-primary">Artists</span>
-            </h3>
-          </div>
-          <Swiper
-            slidesPerView={5}
-            spaceBetween={30}
-            freeMode={true}
-            pagination={{
-              clickable: true,
-            }}
-            autoplay={{
-              delay: 2500,
-              disableOnInteraction: false,
-            }}
-            modules={[FreeMode, Pagination, Autoplay]}
-            className="mySwiper"
-          >
-            {Images.map((item, index) => (
-              <SwiperSlide
-                key={index}
-                role="group"
-                aria-label="1 / 10"
-                className="swiper-slide-active"
-              >
-                <div className="d-block text-center">
-                  <a href="/demo/app/artist/1/details">
-                    <img
-                      className="avatar__image"
-                      src={item.src}
-                      alt={item.name}
-                    />
-                  </a>
-                  <a className="mt-3" href="/demo/app/artist/1/details">
-                    {item.name}
-                  </a>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-        <div className="section">
-          <div className="section__head">
-            <h3 className="mb-0">
               Top <span className="text-primary">Artists</span>
             </h3>
           </div>
           <div className="d-flex flex-wrap justify-content-center cover row g-4">
-            {Images.map((item, index) => (
+            {artists.map((item, index) => (
               <div className="col-6 col-xl-2 col-md-3 col-sm-4 mx-2">
-                <a
+                <Link
                   key={index}
-                  href="artist-details.html"
+                  to={`/artist/${item.id}`}
                   className="cover cover--round "
                 >
                   <div className="cover__image">
-                    <img src={item.src} alt={item.name} />
+                    <img src={item.picture} alt={item.name} />
                   </div>
                   <div className="cover__foot">
                     <span className="cover__title text-truncate">
-                      A{item.name}
+                      {item.name}
                     </span>
                   </div>
-                </a>
+                </Link>
               </div>
             ))}
           </div>
