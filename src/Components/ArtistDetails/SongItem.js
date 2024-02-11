ee5c891892041fad27fe8d4053925a10752a9f93
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-02-11 20:44:03 +0200

    add song to playlist

--- src/Components/ArtistDetails/SongItem.js
+++ src/Components/ArtistDetails/SongItem.js
@@ -1,9 +1,70 @@
+import axios from "axios";
 import "./Artist.css";
 import { useState } from "react";
+import Swal from "sweetalert2";
 
 export default function Song({ cover, preview, title, name, id }) {
-  const handleClick = (id) => {
+  const [my_playlists, setMyPlaylists] = useState([]);
+
+  const handleClick = async (id) => {
     console.log(id);
+    axios.get(`http://localhost:8000/api/playlists`).then(function (res) {
+      console.log(res.data);
+      AddToPlaylist(res.data, id);
+    });
+  };
+
+  const AddToPlaylist = (playlists, songId) => {
+    const inputOptions = playlists.reduce((acc, playlist) => {
+      acc[playlist.id] = playlist.name;
+      return acc;
+    }, {});
+
+    Swal.fire({
+      title: "Submit your Github username",
+      input: "select",
+      inputOptions: inputOptions,
+      inputAttributes: {
+        autocapitalize: "off",
+      },
+      showCancelButton: true,
+      confirmButtonText: "Add",
+      showLoaderOnConfirm: true,
+      preConfirm: async (playlistId) => {
+        return axios
+          .post(`http://localhost:8000/api/addSong`, {
+            songId: songId,
+            playlistId: playlistId,
+          })
+          .then((response) => {
+            if (response.status !== 200 && response.status !== 201) {
+              throw new Error(response.statusText);
+            }
+            return response.data;
+          })
+          .catch((error) => {
+            if (error.response) {
+              Swal.showValidationMessage(
+                `Request failed: ${error.response.data}`
+              );
+            } else if (error.request) {
+              Swal.showValidationMessage(`Request failed: ${error.request}`);
+            } else {
+              Swal.showValidationMessage(`Error: ${error.message}`);
+            }
+          });
+      },
+      allowOutsideClick: () => !Swal.isLoading(),
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          icon: "success",
+          title: "Your work has been saved",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
   };
 
   return (
@@ -39,7 +100,7 @@ export default function Song({ cover, preview, title, name, id }) {
             aria-label="Cover options"
             aria-expanded="false"
           >
-            <i class="bx bx-list-plus"></i>
+            <i className="bx bx-list-plus"></i>
           </a>
         </li>
       </ul>
